7c6977866e5059712d308bb34cee2847ef74319a
Manti Mokone <mokonemanti256@gmail.com>
2025-01-03 21:23:42 -0500

    Backend Done

--- server/taskManagement/api/models.py
+++ server/taskManagement/api/models.py
@@ -4,12 +4,11 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=50,null=False,unique=True)
     description = models.TextField(max_length=500,null=False)
-    start_date = models.DateTimeField(auto_now_add=True,null=False)
-    end_date = models.DateTimeField(auto_now_add=True, null=False)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
     
 
-    
