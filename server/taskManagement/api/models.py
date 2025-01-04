a31e4b1315850d9803951d44390cd57cfe7d8ba2
Manti Mokone <mokonemanti256@gmail.com>
2025-01-03 20:58:10 -0500

    Models Defined

--- server/taskManagement/api/models.py
+++ server/taskManagement/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=50,null=False,unique=True)
+    description = models.TextField(max_length=500,null=False)
+    start_date = models.DateTimeField(auto_now_add=True,null=False)
+    end_date = models.DateTimeField(auto_now_add=True, null=False)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+
+    
