c44b762a00af1deb72c65457965d21fabc456f12
Manti Mokone <mokonemanti256@gmail.com>
2025-01-03 21:10:55 -0500

    URLS configured

--- server/taskManagement/api/urls.py
+++ server/taskManagement/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path,include
 from . import views
+from rest_framework import routers
+from api.views import TaskViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tasks',views.TaskViewSet)
 
 urlpatterns = [
-    path('',views.apiSite)
+    path('',views.apiSite),
+    path('',include(router.urls))
 ]
