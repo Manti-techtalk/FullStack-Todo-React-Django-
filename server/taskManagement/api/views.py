bf4331c3e79d62b632bc0e7fdcca6c581d60cfaa
Manti Mokone <mokonemanti256@gmail.com>
2025-01-03 21:05:58 -0500

    ViewSets Defined

--- server/taskManagement/api/views.py
+++ server/taskManagement/api/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets, permissions
+from api.models import Task
+from api.serializers import TaskSerializer
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 def apiSite(request):
     return HttpResponse ('<h1>Test Test</h1>')
